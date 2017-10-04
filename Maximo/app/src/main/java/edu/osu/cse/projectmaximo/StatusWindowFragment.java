b7de78b695e0f213ec97a99341c36f1d13d1de22
Alex Tanchevski <atanchevski@gmail.com>
2017-10-04 16:15:29 -0400

    Added status window for sensors (incomplete)

---
+++ Maximo/app/src/main/java/edu/osu/cse/projectmaximo/StatusWindowFragment.java
@@ -0,0 +1,42 @@
+package edu.osu.cse.projectmaximo;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * Created by Alex on 10/4/2017.
+ * Fragment used for displaying various sensors and their operational status.
+ * (Temporarily hard-coded in - must change based on number of sensors)
+ */
+
+public class StatusWindowFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //Fill in the status window table with all sensors and their operational status
+        View view = inflater.inflate(R.layout.fragment_status_window, container, false);
+        //Get sensor names and insert into table
+        TextView sensorOne = view.findViewById(R.id.sensorOne);
+        sensorOne.setText(getString(R.string.empty_string, "Sensor 1"));
+        TextView sensorTwo = view.findViewById(R.id.sensorTwo);
+        sensorTwo.setText(getString(R.string.empty_string, "Sensor 2"));
+
+        //Get operational status of each sensor
+
+        //Fill in operational status of each sensor
+        TextView statusOne = view.findViewById(R.id.statusOne);
+        statusOne.setText(getString(R.string.empty_string, "Operational"));
+        TextView statusTwo = view.findViewById(R.id.statusTwo);
+        statusTwo.setText(getString(R.string.empty_string, "Down"));
+
+        return view;
+    }
+
+}
