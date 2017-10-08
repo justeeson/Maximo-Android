72d01c5ca122e69d69a54e46d23075e04763d159
Jon Seaman <jonmseaman@live.com>
2017-10-08 12:54:08 -0400

    Added logic for changing chat view color

--- Maximo/app/src/main/java/edu/osu/cse/projectmaximo/ChatBot/ChatMessageView.java
+++ Maximo/app/src/main/java/edu/osu/cse/projectmaximo/ChatBot/ChatMessageView.java
@@ -1,8 +1,11 @@
 package edu.osu.cse.projectmaximo.ChatBot;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -23,5 +26,29 @@ public class ChatMessageView extends ConstraintLayout {
         inflate(getContext(), R.layout.chatbot_chatmessage_view, this);
     }
 
+    /**
+     * Adjusts the styling of this chat message to be a request to the chat bot.
+     */
+    public void makeRequest() {
+        setColor(R.color.colorChatRequest);
+    }
 
+    /**
+     * Adjusts the styling of this chat message to be a respone to the chat bot.
+     */
+    public void makeResponse() {
+        setColor(R.color.colorChatResponse);
+
+    }
+
+    private void setColor(int color) {
+        // Should have only one child, but just in case.
+        for (int i = 0; i < this.getChildCount(); i++) {
+            // Set chat_message_text color.
+            View child = this.getChildAt(i);
+            if (child.getId() == R.id.chat_message_text) {
+                child.setBackgroundColor(color);
+            }
+        }
+    }
 }
