8c47564cfb9fdccf5606e86e68d0f53ac71e43f8
Momo Liu <momoliu@momodembp.attlocal.net>
2017-10-08 17:26:52 -0400

    created a table in DB for work items and list view for work items

--- Maximo/app/src/main/java/edu/osu/cse/projectmaximo/Maximo.java
+++ Maximo/app/src/main/java/edu/osu/cse/projectmaximo/Maximo.java
@@ -2,16 +2,19 @@ package edu.osu.cse.projectmaximo;
 
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.content.Intent;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,99 +37,149 @@ public class Maximo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(getApplicationContext());
         SensorGaugeReaderDbHelper sDbHelper = new SensorGaugeReaderDbHelper(getApplicationContext());
+        WorkItemsDbHelper wDbHelper = new WorkItemsDbHelper(getApplicationContext());
 
         // Gets the data repository in write mode
         SQLiteDatabase dbWriteable = mDbHelper.getWritableDatabase();
         SQLiteDatabase sdbWriteable = sDbHelper.getWritableDatabase();
+        SQLiteDatabase wdbWriteable = wDbHelper.getWritableDatabase();
 
         // Create a new map of values, where column names are the keys
         ContentValues values = new ContentValues();
-        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_USERID, "1");
-        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_USERNAME, "userone");
-        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_FIRSTNAME, "Mike");
-        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_LASTNAME, "Rowsoft");
+//        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_USERID, "1");
+//        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_USERNAME, "userone");
+//        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_FIRSTNAME, "Mike");
+//        values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_LASTNAME, "Rowsoft");
+//
+//        // Insert the new row, returning the primary key value of the new row
+//        long newRowId = dbWriteable.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values);
+//
+//        //Sensor Gauge Sensor 1
+//        values = new ContentValues();
+//        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORID, "1");
+//        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORNAME, "Sensor 1");
+//        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORTOTALVALUE, "500");
+//        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORACTUALVALUE, "325");
+//
+//        newRowId = dbWriteable.insert(SensorGaugeReaderContract.FeedEntry.TABLE_NAME, null, values);
+//
+//
+//        //Sensor Gauge Sensor 2
+//        values = new ContentValues();
+//        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORID, "2");
+//        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORNAME, "Sensor 2");
+//        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORTOTALVALUE, "700");
+//        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORACTUALVALUE, "405");
+//
+//        newRowId = dbWriteable.insert(SensorGaugeReaderContract.FeedEntry.TABLE_NAME, null, values);
 
-        // Insert the new row, returning the primary key value of the new row
-        long newRowId = dbWriteable.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values);
-
-        //Sensor Gauge Sensor 1
+        // Work Items
         values = new ContentValues();
-        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORID, "1");
-        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORNAME, "Sensor 1");
-        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORTOTALVALUE, "500");
-        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORACTUALVALUE, "325");
+        values.put(WorkItemsContract.WorkItemsEntry.COLUMN_NAME_ITEM, "item1");
 
-        newRowId = dbWriteable.insert(SensorGaugeReaderContract.FeedEntry.TABLE_NAME, null, values);
+        long newRowId = wdbWriteable.insert(WorkItemsContract.WorkItemsEntry.TABLE_NAME, null, values);
 
-
-        //Sensor Gauge Sensor 2
-        values = new ContentValues();
-        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORID, "2");
-        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORNAME, "Sensor 2");
-        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORTOTALVALUE, "700");
-        values.put(SensorGaugeReaderContract.FeedEntry.COLUMN_NAME_SENSORACTUALVALUE, "405");
-
-        newRowId = dbWriteable.insert(SensorGaugeReaderContract.FeedEntry.TABLE_NAME, null, values);
 
         //Get Readable versions of both databases
         SQLiteDatabase dbReadable = mDbHelper.getReadableDatabase();
         SQLiteDatabase sdbReadable = sDbHelper.getReadableDatabase();
+        SQLiteDatabase wdbReadable = wDbHelper.getReadableDatabase();
 
         // Define a projection that specifies which columns from the database
         // you will actually use after this query.
-        String[] projection = {
-                FeedReaderContract.FeedEntry._ID,
-                FeedReaderContract.FeedEntry.COLUMN_NAME_USERID,
-                FeedReaderContract.FeedEntry.COLUMN_NAME_USERNAME,
-                FeedReaderContract.FeedEntry.COLUMN_NAME_FIRSTNAME,
-                FeedReaderContract.FeedEntry.COLUMN_NAME_LASTNAME
-        };
+//        String[] projection = {
+//                FeedReaderContract.FeedEntry._ID,
+//                FeedReaderContract.FeedEntry.COLUMN_NAME_USERID,
+//                FeedReaderContract.FeedEntry.COLUMN_NAME_USERNAME,
+//                FeedReaderContract.FeedEntry.COLUMN_NAME_FIRSTNAME,
+//                FeedReaderContract.FeedEntry.COLUMN_NAME_LASTNAME
+//        };
+//
+//        // Filter results WHERE "title" = 'My Title'
+//        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_FIRSTNAME + " = ?";
+//        String[] selectionArgs = {"User"};
+//
+//        // How you want the results sorted in the resulting Cursor
+//        String sortOrder =
+//                FeedReaderContract.FeedEntry.COLUMN_NAME_USERID + " ASC";
+//
+//        Cursor cursor = dbReadable.query(
+//                FeedReaderContract.FeedEntry.TABLE_NAME,                     // The table to query
+//                projection,                               // The columns to return
+//                selection,                                // The columns for the WHERE clause
+//                selectionArgs,                            // The values for the WHERE clause
+//                null,                                     // don't group the rows
+//                null,                                     // don't filter by row groups
+//                sortOrder                                 // The sort order
+//        );
+//
+//        // Read in the rows with the cursor
+//        List<String> itemIds = new ArrayList<String>();
+//        while (cursor.moveToNext()) {
+//            String itemId = cursor.getString(
+//                    cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_FIRSTNAME));
+//            itemIds.add(itemId);
+//        }
+//        cursor.close();
+//        userIdentity = " " + itemIds.get(0);
 
-        // Filter results WHERE "title" = 'My Title'
-        String selection = FeedReaderContract.FeedEntry.COLUMN_NAME_FIRSTNAME + " = ?";
-        String[] selectionArgs = { "User" };
+        //read work items
+        String[] workItems_projection = {
+                WorkItemsContract.WorkItemsEntry.COLUMN_NAME_ITEM
+        };
+        String selection = WorkItemsContract.WorkItemsEntry.COLUMN_NAME_ITEM + " = ?";
+       // String[] workitem_selectionArgs = {"Current Items"};
 
         // How you want the results sorted in the resulting Cursor
         String sortOrder =
-                FeedReaderContract.FeedEntry.COLUMN_NAME_USERID + " ASC";
+                WorkItemsContract.WorkItemsEntry.COLUMN_NAME_ITEM + " DESC";
 
-        Cursor cursor = dbReadable.query(
-                FeedReaderContract.FeedEntry.TABLE_NAME,                     // The table to query
-                projection,                               // The columns to return
+        Cursor cursor = wdbReadable.query(
+                WorkItemsContract.WorkItemsEntry.TABLE_NAME,                     // The table to query
+                workItems_projection,                               // The columns to return
                 selection,                                // The columns for the WHERE clause
-                selectionArgs,                            // The values for the WHERE clause
+                null,                                     // The values for the WHERE clause
                 null,                                     // don't group the rows
                 null,                                     // don't filter by row groups
                 sortOrder                                 // The sort order
         );
-
-        // Read in the rows with the cursor
-        List<String> itemIds = new ArrayList<String>();
+        List workitem_list = new ArrayList<>();
         while(cursor.moveToNext()) {
             String itemId = cursor.getString(
-                    cursor.getColumnIndexOrThrow(FeedReaderContract.FeedEntry.COLUMN_NAME_FIRSTNAME));
-            itemIds.add(itemId);
+                    cursor.getColumnIndexOrThrow(WorkItemsContract.WorkItemsEntry._ID));
+            workitem_list.add(itemId);
         }
         cursor.close();
-        userIdentity = " " + itemIds.get(0);
+        //Log.i("work item read",workitem_list.toString());
+        Log.i("work item read",workitem_list.toString());
 
         // We're calling this last so the name can be pulled before
         // the screen is created
         setContentView(R.layout.activity_maximo);
 
-
         //set intent
-        Button media_dash  = (Button)findViewById(R.id.MediaDashboardBtn);
+        Button media_dash = (Button) findViewById(R.id.MediaDashboardBtn);
         media_dash.setOnClickListener(
-                new Button.OnClickListener()
-                {
-                    public void onClick(View view){
+                new Button.OnClickListener() {
+                    public void onClick(View view) {
 
-                        Intent intent_to_media =  new Intent(view.getContext(), MediaDashboardActivity.class);
+                        Intent intent_to_media = new Intent(view.getContext(), MediaDashboardActivity.class);
                         startActivity(intent_to_media);
                     }
                 }
         );
+        //displayWorkItems();
+
+    }
+
+    public void displayWorkItems() {
+        //get work items list
+        String[] work_items = {"item1", "item2"};
+        ArrayAdapter<String> work_item_adapter = new ArrayAdapter<String>(this, R.layout.activity_maximo, work_items);
+        /////////// cannot find work_item_list
+        ListView work_item_list = (ListView) findViewById(R.id.WorkItemList);
+        work_item_list.setAdapter(work_item_adapter);
+
     }
 
     @Override
---
+++ Maximo/app/src/main/java/edu/osu/cse/projectmaximo/WorkItemsDbHelper.java
@@ -0,0 +1,34 @@
+package edu.osu.cse.projectmaximo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by momoliu on 10/6/17.
+ */
+
+public class WorkItemsDbHelper extends SQLiteOpenHelper{
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "WorkItemsDb.db";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + WorkItemsContract.WorkItemsEntry.TABLE_NAME + " (" +
+                    WorkItemsContract.WorkItemsEntry._ID + " INTEGER PRIMARY KEY," +
+                    WorkItemsContract.WorkItemsEntry.COLUMN_NAME_ITEM + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + WorkItemsContract.WorkItemsEntry.TABLE_NAME;
+
+    public WorkItemsDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+     @Override
+    public void onCreate(SQLiteDatabase db) {db.execSQL(SQL_CREATE_ENTRIES);}
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+
+}
