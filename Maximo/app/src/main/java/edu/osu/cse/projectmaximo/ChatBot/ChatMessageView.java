dc35bedf50b6a638ea9bb70a5b783efc515a4358
Jon Seaman <jonmseaman@live.com>
2017-10-08 13:11:38 -0400

    Logic for adjusting alignment

--- Maximo/app/src/main/java/edu/osu/cse/projectmaximo/ChatBot/ChatMessageView.java
+++ Maximo/app/src/main/java/edu/osu/cse/projectmaximo/ChatBot/ChatMessageView.java
@@ -5,6 +5,7 @@ import android.content.res.Resources;
 import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
@@ -31,6 +32,7 @@ public class ChatMessageView extends ConstraintLayout {
      */
     public void makeRequest() {
         setColor(R.color.colorChatRequest);
+        setAlignment(ConstraintSet.RIGHT);
     }
 
     /**
@@ -38,11 +40,23 @@ public class ChatMessageView extends ConstraintLayout {
      */
     public void makeResponse() {
         setColor(R.color.colorChatResponse);
+        setAlignment(ConstraintSet.LEFT);
     }
 
     private void setColor(int color) {
         // Set the background color for the text box.
         View msgText = this.findViewById(R.id.chat_message_text);
         msgText.setBackgroundColor(color);
+    }
+
+    private void setAlignment(int alignment) {
+        View msgText = this.findViewById(R.id.chat_message_text);
+
+        // TODO: Test this.
+        int endSide = alignment == ConstraintSet.LEFT ? ConstraintSet.RIGHT : ConstraintSet.LEFT;
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.connect(msgText.getId(), alignment, ConstraintSet.PARENT_ID, endSide);
+
     }
 }
