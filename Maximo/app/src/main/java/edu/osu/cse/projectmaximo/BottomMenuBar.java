c5bee2cc2b01d153717c360aaa614ab72050c1af
Alex Tanchevski <atanchevski@gmail.com>
2017-10-16 17:54:10 -0400

    Added functionality to navbar on main dashboard

--- Maximo/app/src/main/java/edu/osu/cse/projectmaximo/BottomMenuBar.java
+++ Maximo/app/src/main/java/edu/osu/cse/projectmaximo/BottomMenuBar.java
@@ -3,29 +3,30 @@ package edu.osu.cse.projectmaximo;
 import android.content.Intent;
 import android.view.View;
 
+import edu.osu.cse.projectmaximo.ChatBot.ChatBotActivity;
+
 /**
  * Created by Alex on 10/12/2017.
  * Bottom navigation bar for each dashboard.
  */
 
-public class BottomMenuBar extends Maximo implements View.OnClickListener{
+public class BottomMenuBar{
 
     /**
      * Handles click events for the bottom navigation bar.
      * @param v the pressed navbar button
      */
-    @Override
-    public void onClick(View v) {
+    public static void menuClick(View v) {
         // Find which button was clicked. Call appropriate activity
         switch (v.getId()) {
             case R.id.home_button: // Home
-                startActivity(new Intent(this, Maximo.class));
+                v.getContext().startActivity(new Intent(v.getContext(), Maximo.class));
                 break;
             case R.id.media_dashboard_btn: // Media
-                startActivity(new Intent(this, MediaDashboardActivity.class));
+                v.getContext().startActivity(new Intent(v.getContext(), MediaDashboardActivity.class));
                 break;
             case R.id.chat_btn: // Chat
-                startActivity(new Intent(this, MainActivity.class));
+                v.getContext().startActivity(new Intent(v.getContext(), ChatBotActivity.class));
                 break;
             default:
                 break;
--- Maximo/app/src/main/java/edu/osu/cse/projectmaximo/Maximo.java
+++ Maximo/app/src/main/java/edu/osu/cse/projectmaximo/Maximo.java
@@ -114,6 +114,28 @@ public class Maximo extends AppCompatActivity {
         // the screen is created
         setContentView(R.layout.activity_maximo);
 
+        //bottom navbar menu button functionality
+        final Button homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                BottomMenuBar.menuClick(v);
+            }
+        });
+
+        final Button mediaButton = findViewById(R.id.media_dashboard_btn);
+        mediaButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                BottomMenuBar.menuClick(v);
+            }
+        });
+
+        final Button chatButton = findViewById(R.id.chat_btn);
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                BottomMenuBar.menuClick(v);
+            }
+        });
+
     }
 
     @Override
