3a2d65bea963940e47929f77e8e5c7a888ac2038
Momo Liu <momoliu@momodembp.attlocal.net>
2017-10-19 13:41:38 -0400

    fixed the bug that cannot show the usename in the main dashboard

--- Maximo/app/src/main/java/edu/osu/cse/projectmaximo/FeedReaderDbHelper.java
+++ Maximo/app/src/main/java/edu/osu/cse/projectmaximo/FeedReaderDbHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class FeedReaderDbHelper extends SQLiteOpenHelper {
     // If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 3;
+    public static final int DATABASE_VERSION = 4;
     public static final String DATABASE_NAME = "FeedReader.db";
 
     public FeedReaderDbHelper(Context context) {
