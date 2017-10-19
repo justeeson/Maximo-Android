12c1b15ad7e35b1a6b68b824281d89a5c6389e7e
Momo Liu <momoliu@momodembp.attlocal.net>
2017-10-19 11:46:02 -0400

    fixed media dashboard so that it has a header at the top. Also added some ui stuff for media dashboard so it looks better. Changed the media dashboard structure for future extenstion.

--- Maximo/app/src/main/java/edu/osu/cse/projectmaximo/MediaDashboardActivity.java
+++ Maximo/app/src/main/java/edu/osu/cse/projectmaximo/MediaDashboardActivity.java
@@ -1,32 +1,58 @@
 package edu.osu.cse.projectmaximo;
 
-import android.app.ListActivity;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class MediaDashboardActivity extends ListActivity {
+public class MediaDashboardActivity extends AppCompatActivity {
+
+    private ListView media_assets_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_media_dashboard);
+        setContentView(R.layout.activity_media_dashboard);
 
+        pupulateMediaAssetsList();
+    }
+
+    public void pupulateMediaAssetsList(){
         // get files' names from asset directory
         // if the file is empty, an alert message would display
-        String[] list, media_asset = {"No available media asset!"};
+        String[] list, final_media_assets = {"No available media asset!"};
+        ArrayList<String> media_assets = new ArrayList<String>();
+        ArrayList<String> filetype = new ArrayList<String>();
+        ArrayList<String> size = new ArrayList<String>();
         try {
-            list = getAssets().list("");
+            AssetManager assetManager = getAssets();
+            list = assetManager.list("");
+            // remove all the built-in files in Android, only show user's own assets
             if (list.length > 0) {
-                media_asset = list;
+                for(int i = 0; i < list.length; i++){
+                    if(!list[i].equals("images") && !list[i].equals("sounds") && !list[i].equals("webkit")){
+                        //put filename in {media_assets}
+                        String file = list[i];
+                        media_assets.add(list[i].substring(0, list[i].indexOf('.')));
+                        //put filetype in {filetype}
+                        filetype.add(list[i].substring(list[i].indexOf('.')+1,list[i].length()));
+                    }
+                }
+                final_media_assets = media_assets.toArray(new String[media_assets.size()]);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
         // display media asset files name on screen
-        ArrayAdapter<String> media_adapter = new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1, media_asset);
-        getListView().setAdapter(media_adapter);
+//        ArrayAdapter<String> media_adapter = new ArrayAdapter<String>(getListView().getContext(), R.layout.activity_media_dashboard, final_media_assets);
+//        getListView().setAdapter(media_adapter);
+        ArrayAdapter<String> media_adapter = new ArrayAdapter<String>(this, R.layout.media_assets_list, final_media_assets);
+        media_assets_list = (ListView)findViewById(R.id.media_list);
+        media_assets_list.setAdapter(media_adapter);
     }
 }
