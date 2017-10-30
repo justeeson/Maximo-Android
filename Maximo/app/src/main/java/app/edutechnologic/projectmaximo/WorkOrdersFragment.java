a2806b09f4d65c5171bee9bcb713a588efd34cfd
Momo Liu <momoliu@momodembp.attlocal.net>
2017-10-29 22:43:34 -0400

    populate data from database and display them as a table on work orders dashboard

--- Maximo/app/src/main/java/app/edutechnologic/projectmaximo/WorkOrdersFragment.java
+++ Maximo/app/src/main/java/app/edutechnologic/projectmaximo/WorkOrdersFragment.java
@@ -1,6 +1,7 @@
 package app.edutechnologic.projectmaximo;
 
 import android.app.Fragment;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +11,8 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by Alex on 10/26/2017.
  * Fragment used for displaying the work orders table.
@@ -18,24 +21,98 @@ import android.widget.TextView;
 public class WorkOrdersFragment extends Fragment {
 
     TableLayout tableLayout;
-    TableRow tableRow;
+    ArrayList<String> work_order_number = new ArrayList<String>();
+    ArrayList<String> work_order_descriptions = new ArrayList<String>();
+    ArrayList<String> work_order_assetnumbers = new ArrayList<String>();
+    ArrayList<String> work_order_locations = new ArrayList<String>();
+    ArrayList<String> work_order_reporteddates = new ArrayList<String>();
+    ArrayList<String> work_order_statuses = new ArrayList<String>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_work_orders_table, container, false);
         tableLayout = (TableLayout) view.findViewById(R.id.workOrdersTable);
-        tableRow = new TableRow(getActivity());      // new row
-        TextView b1 = new TextView(getActivity());
-        b1.setText("I'm added dynamically");
-        tableRow.addView(b1);
-        tableLayout.addView(tableRow);
-        //Populate work orders table
+
+        readData();
+//        work_order_number.add("number1");
+//        work_order_number.add("number2");
+
+        TableRow[] tableRow = new TableRow[work_order_number.size()];
+        TextView[] textView = new TextView[6];
+        for (int i = 0; i < work_order_number.size(); i++) {
+            tableRow[i] = new TableRow(getActivity());
+
+            // set text for columns
+            textView[0] = new TextView(getActivity());
+            textView[0].setText(work_order_number.get(i));
+
+            textView[1] = new TextView(getActivity());
+            textView[1].setText(work_order_descriptions.get(i));
+
+            textView[2] = new TextView(getActivity());
+            textView[2].setText(work_order_assetnumbers.get(i));
+
+            textView[3] = new TextView(getActivity());
+            textView[3].setText(work_order_locations.get(i));
+
+            textView[4] = new TextView(getActivity());
+            textView[4].setText(work_order_reporteddates.get(i));
+
+            textView[5] = new TextView(getActivity());
+            textView[5].setText(work_order_statuses.get(i));
+
+            // add columns in the current row
+            for (int j = 0; j < 6; j++) {
+                tableRow[i].addView(textView[j], new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
+            }
+
+            // add current in the table
+            tableLayout.addView(tableRow[i]);
+        }
 
         return view;
     }
 
     public void readData() {
-        WorkOrderDbHelper wDbHelper = new WorkOrderDbHelper(getActivity()); // 待确认
+        WorkOrderDbHelper wDbHelper = new WorkOrderDbHelper(getActivity());
         SQLiteDatabase wdbReadable = wDbHelper.getReadableDatabase();
+
+        /**
+         * read work orders
+         *
+         * */
+        // Cursor cursor = wdbReadable.rawQuery("select " + WorkOrderContract.WorkOrderEntry.COLUMN_NAME_NUMBER + "from " + WorkOrderContract.WorkOrderEntry.TABLE_NAME, null);
+        Cursor cursor = wdbReadable.rawQuery("select * from " + WorkOrderContract.WorkOrderEntry.TABLE_NAME, null);
+
+        //work_order_number = new ArrayList<String> ();
+        while (cursor.moveToNext()) {
+            String item_number = cursor.getString(
+                    cursor.getColumnIndexOrThrow(WorkOrderContract.WorkOrderEntry.COLUMN_NAME_NUMBER));
+            work_order_number.add(item_number);
+
+            String item_descriptions = cursor.getString(
+                    cursor.getColumnIndexOrThrow(WorkOrderContract.WorkOrderEntry.COLUMN_NAME_DESCRIPTION));
+            work_order_descriptions.add(item_descriptions);
+
+            String item_assetnumbers = cursor.getString(
+                    cursor.getColumnIndexOrThrow(WorkOrderContract.WorkOrderEntry.COLUMN_NAME_ASSETNUMBER));
+            work_order_assetnumbers.add(item_assetnumbers);
+
+            String item_locations = cursor.getString(
+                    cursor.getColumnIndexOrThrow(WorkOrderContract.WorkOrderEntry.COLUMN_NAME_LOCATION));
+            work_order_locations.add(item_locations);
+
+            String item_reporteddates = cursor.getString(
+                    cursor.getColumnIndexOrThrow(WorkOrderContract.WorkOrderEntry.COLUMN_NAME_REPORTEDDATE));
+            work_order_reporteddates.add(item_reporteddates);
+
+            String item_statuses = cursor.getString(
+                    cursor.getColumnIndexOrThrow(WorkOrderContract.WorkOrderEntry.COLUMN_NAME_STATUS));
+            work_order_statuses.add(item_statuses);
+
+        }
+
+        cursor.close();
     }
 }
