e7af705d159a4777eaefb8e4a997c41b160de0cd
Jon Seaman <jonmseaman@live.com>
2017-10-30 18:50:52 -0400

    Work on fetching work items

--- Maximo/app/src/main/java/app/edutechnologic/projectmaximo/ChatBot/ChatBotHandler.java
+++ Maximo/app/src/main/java/app/edutechnologic/projectmaximo/ChatBot/ChatBotHandler.java
@@ -118,15 +118,13 @@ public class ChatBotHandler{
 
                     System.out.println(response);
 
-                    // TODO: HandleAction(response)
-
                     // Obtain response from Watson API
                     ArrayList responseList = (ArrayList) response.getOutput().get("text");
                     if (null != responseList && responseList.size() > 0) {
                         replyFromWatson = ((String) responseList.get(0));
-                        //String response = IntentHandler.handleIntent(response);
-                        responseFromWatson.setWatsonMessage(replyFromWatson);
-
+                        String localResponse = IntentHandler.handleIntent(response);
+                        // FIXME: This is gross. Call a method to add something to chat.
+                        responseFromWatson.setWatsonMessage(replyFromWatson + "\n" + localResponse);
                     }
 
                 } catch (Exception e) {
--- Maximo/app/src/main/java/app/edutechnologic/projectmaximo/ChatBot/Response/IntentHandler.java
+++ Maximo/app/src/main/java/app/edutechnologic/projectmaximo/ChatBot/Response/IntentHandler.java
@@ -11,9 +11,17 @@ import org.apache.commons.lang3.NotImplementedException;
 public class IntentHandler {
     private static ActionPovider actionProvider;
 
+    // TODO: Set a default intent handler
+
     // Process intent handling capabilities
     public static String handleIntent(MessageResponse response) {
-        throw new NotImplementedException();
+        if (response.getIntents().contains("FetchWorkItems")) {
+            return "Work item1: ... // TODO: implement this";
+        }
+        // If is work items
+            // Fetch from the DB and return the work items.
+        return response.getText().get(0);
+        //throw new NotImplementedException();
     }
 
     // This would allow the pogram to execute an action provider through setting.
